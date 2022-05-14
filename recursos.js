f0a768adc3810fb293d53e406afcfa191352024b
Karim <karim.gonzalez.ait@gmail.com>
2022-05-14 14:22:56 +0200

    update recursos, añadido hierro y oro

--- recursos.js
+++ recursos.js
@@ -9,8 +9,12 @@ let edTier2 = document.getElementById("edTier2")   // +2 D20
 
 let terrenoUno = document.getElementById("terrenoUno") // +1 D20
 let terrenoDos = document.getElementById("terrenoDos")  // +2 D20
-let hierro = document.getElementById("hierro");
-let oro = document.getElementById("oro");
+
+let hierro = document.getElementById("hierroTier1");
+let hierroDos = document.getElementById("hierroTier2");
+
+let oro = document.getElementById("oroTier1");
+let oroDos = document.getElementById("oroTier2");
 
 
 let recursoFinal = document.getElementById("recursoFinal")
@@ -27,12 +31,14 @@ let dadoDadoTerreno = document.getElementById("dadoDadoTerreno");
 // BOTON
 
 let randomD20 = () => Math.floor(Math.random()*20)+1;
+let randomD10 = () => Math.floor(Math.random()*10)+1
+let randomD6 = () => Math.floor(Math.random()*6)+1
 
 const boniTerreno = () => {
   if(llanura.checked === true){
-    return recursoTotal * 2
+    return recursoTotal * 1
   } else if (bosque.checked === true){
-    return recursoTotal * 3
+    return recursoTotal * 2
   }
 }
 
@@ -62,19 +68,36 @@ const dadoTerreno = () => {
     let y = randomD20()
     dadoDadoTerreno.innerHTML = x + y
     return x + y
-  } 
-  else if(hierro.checked === true)
-  {
-    let x = Math.floor(Math.random()*10)+1
-    dadoDadoTerreno.innerHTML = x
-    return x
-  } 
-  else if(oro.checked === true)
-  {
-    let x = Math.floor(Math.random()*6)+1
-    dadoDadoTerreno.innerHTML = x
-    return x
   }
+}
+
+const metalFunction = () => {
+  if(hierro.checked === true)
+    {
+      let x = randomD10()
+      dadoHierro.innerHTML = x;
+      return x;
+    }
+    else if(oro.checked === true){
+      let x = randomD6()
+      dadoOro.innerHTML = x;
+      return x;
+    }
+    else if(hierroDos.checked === true)
+    {
+      let x = randomD10()
+      let y = randomD10()
+      dadoHierro.innerHTML = x + y
+      return x + y
+      
+    } 
+    else if(oroDos.checked === true)
+    {
+      let x = randomD6()
+      let y = randomD6()
+      dadoOro.innerHTML = x + y
+      return x + y
+    } 
 }
 
 buttonCalcular.addEventListener("click", function(){
@@ -85,34 +108,76 @@ buttonCalcular.addEventListener("click", function(){
 
   let countLeñadores =  Math.floor(recursoTotal / 10)
   
-  if(hierro.checked === true)
-  {
-    for(let i=0; i < countLeñadores; i++){
-      let x = Math.floor(Math.random()*10)+1
-      recursoTotal += x;
-      dadosBonus += x;
-    }
+  
+  
+  if(llanura.checked || bosque.checked){
+    recursoTotal += boniTerreno()
+    
+  }
+
+  if(edTier1.checked || edTier2.checked){
+    recursoTotal += edificios()
+  }
+
+  if(terrenoUno.checked || terrenoDos.checked){
+    recursoTotal += dadoTerreno()
   } 
-    else if(oro.checked === true)
-    {
-      for(let i=0; i < countLeñadores; i++){
-        let x = Math.floor(Math.random()*6)+1
-        recursoTotal += x;
-        dadosBonus += x;
-      }
-    } 
-      else 
-      {
-        for(let i=0; i < countLeñadores; i++){
-          let x = randomD20()
-          recursoTotal += x;
-          dadosBonus += x;
-        }
-      }
+
+  if(hierro.checked || hierroDos.checked)
+  {
+    recursoTotal += metalFunction();
+  } 
+
+  if(oro.checked || oroDos.checked)
+  {
+    recursoTotal += metalFunction();
+
+  }
   
 
-  dadoTrabajador.innerHTML = dadosBonus
-  recursoFinal.innerHTML = (recursoTotal+boniTerreno()+edificios()+dadoTerreno())
+
+  for(let i=0; i < countLeñadores; i++)
+  {
+    if(!hierro.checked || !oro.checked || !hierroDos.checked || !oroDos.checked)
+    {
+      let x = randomD20()
+      recursoTotal += x;
+      dadosBonus += x;
+      dadoTrabajador.innerHTML = dadosBonus
+    } 
+    else if(hierro.checked)
+    {
+      let x = randomD10()
+      recursoTotal += x;
+      dadosBonus += x;
+      dadoHierro.innerHTML = x;
+    }
+    else if(oro.checked){
+      let x = randomD6()
+      recursoTotal += x;
+      dadosBonus += x;
+      dadoOro.innerHTML = x;
+    }
+    else if(hierroDos.cheked)
+    {
+      let x = randomD10()
+      let y = randomD10()
+      dadoHierro.innerHTML = x + y
+      recursoTotal += x
+      recursoTotal += y
+    } 
+    else if(oroDos.checked)
+    {
+      let x = randomD6()
+      let y = randomD6()
+      dadoOro.innerHTML = x + y
+      recursoTotal += x
+      recursoTotal += y
+    } 
+  }
+
+  
+  recursoFinal.innerHTML = (recursoTotal)
  
 })
 
